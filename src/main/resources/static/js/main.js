45d1d4d021318ee3bf52ff1d946f89ef2d5469e6
bartek9105 <bartek9105@gmail.com>
2020-01-21 12:57:22 +0100

    add axios, get products

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const getProducts = () => {
+    axios.get('/api/books/book/1').then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+console.log(getProducts());
