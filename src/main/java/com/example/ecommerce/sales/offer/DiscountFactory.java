a293d283954bdc509ca8d2acfa6fc0f643d12b6d
bartek9105 <bartek9105@gmail.com>
2020-02-26 18:01:35 +0100

    fix

--- src/main/java/com/example/ecommerce/sales/offer/DiscountFactory.java
+++ src/main/java/com/example/ecommerce/sales/offer/DiscountFactory.java
@@ -1,11 +1,7 @@
 package com.example.ecommerce.sales.offer;
 
 public class DiscountFactory {
-    private static DiscountPolicy quantityDiscount() {
-        return new QuantityDiscount(5, 0.20);
-    }
-
-    private static DiscountPolicy poppyAndJazzyDiscount() {
+    static DiscountPolicy quantityDiscount() {
         return new QuantityDiscount(5, 0.20);
     }
 }
